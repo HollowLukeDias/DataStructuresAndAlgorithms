f87a12b0cf617d5717352cc15a7bf5710d111617
LukeDias <lhollowwizard@gmail.com>
2020-12-28 19:25:04 -0300

    Did the Rotate Array to the Right challenge, but it is too slow right now

---
+++ DataStructuresAndAlgorithms/CodingChallenges/RotateArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.CodingChallenges
+{
+    class RotateArray
+    {
+        public static int[] Rotate(int[] array, int steps)
+        {
+            var length = array.Length;
+            var modSteps = steps % length;
+            for (int i = 0; i < modSteps; i++)
+            {
+                var temp = array[length - 1];
+                for (int j = length - 1; j > 0 ; j--)
+                {
+                    array[j] = array[j - 1];
+                }
+                array[0] = temp;
+            }
+            return array;
+        }
+    }
+}
