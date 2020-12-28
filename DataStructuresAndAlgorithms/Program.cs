08bacd25eb6147afdda1413f6e753c231a9943c0
LukeDias <lhollowwizard@gmail.com>
2020-12-28 20:41:35 -0300

    Did a faster way on my own, then 3 different ways from the solutions tab of the challenge, but translating to C#.

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -9,9 +9,9 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main()
         {
-            var integers1 = new int[] { 1, 2, 3, 4, 5 };
+            var integers1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
             var integers2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            var integers3 = new int[] { 1, 2, 3, 4, 5 };
+            var integers3 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
             var integers4 = new int[] { 1, 2, 3, 4, 5 };
             //integers1 = RotateArray.Rotate(integers1, 3);
             //foreach (var integer in integers1)
@@ -19,8 +19,19 @@ namespace DataStructuresAndAlgorithms
             //    Console.Write(integer + ", ");
             //}
             //Console.WriteLine();
-            integers2 = RotateArray.Rotate(integers2, 35);
-            foreach (var integer in integers2)
+            //integers2 = RotateArray.RotateSlow(integers2, 35);
+            //foreach (var integer in integers2)
+            //{
+            //    Console.Write(integer + ", ");
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------------------------------------");
+            //Console.WriteLine();
+
+            integers3 = RotateArray.RotateFast(integers3, 3);
+            foreach (var integer in integers3)
             {
                 Console.Write(integer + ", ");
             }
