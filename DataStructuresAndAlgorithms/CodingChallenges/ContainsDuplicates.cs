f47398803a4ac80e5e808315aaee3607c9969026
LukeDias <lhollowwizard@gmail.com>
2020-12-29 05:29:29 -0300

    Implemented my own version of Single Linked List, didn't do TDD on this one, but I surely should have

---
+++ DataStructuresAndAlgorithms/CodingChallenges/ContainsDuplicates.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.CodingChallenges
+{
+    class ContainsDuplicates
+    {
+
+        // O(n^2) - Time Complexity - Bad
+        // O(1)   - Space Complexity - Great
+        // Nested Loops
+        public static bool ContainsDuplicatesBad(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1;  j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // O(n) - Time Complexity - Good
+        // O(n) - Space Complexity - Good
+        // Hash
+        public static bool ContainsDuplicateGood(int[] array)
+        {
+            var hash = new HashSet<int>();
+            foreach (var number in array)
+            {
+                if (hash.Contains(number))
+                    return true;
+
+                hash.Add(number);
+            }
+
+            return false;
+        }
+
+
+        // O(N * Log N ) - Time Complexity - Fair
+        // O(1)          - Space Complexity - Great
+        //Sorting
+        public static bool ContainsDuplicateFair(int[] array)
+        {
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length - 1; i++)
+                if (array[i] == array[i + 1]) return true;
+
+            return false;
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructuresAndAlgorithms.CodingChallenges;
 using DataStructuresAndAlgorithms.Hash;
+using DataStructuresAndAlgorithms.LinkedList;
 using System;
 using System.Collections.Generic;
 
@@ -9,32 +10,7 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main()
         {
-            var integers1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            var integers2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            var integers3 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
-            var integers4 = new int[] { 1, 2, 3, 4, 5 };
-            //integers1 = RotateArray.Rotate(integers1, 3);
-            //foreach (var integer in integers1)
-            //{
-            //    Console.Write(integer + ", ");
-            //}
-            //Console.WriteLine();
-            //integers2 = RotateArray.RotateSlow(integers2, 35);
-            //foreach (var integer in integers2)
-            //{
-            //    Console.Write(integer + ", ");
-            //}
 
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine("-------------------------------------------------------------");
-            //Console.WriteLine();
-
-            integers3 = RotateArray.RotateFast(integers3, 3);
-            foreach (var integer in integers3)
-            {
-                Console.Write(integer + ", ");
-            }
         }
     }
 }
