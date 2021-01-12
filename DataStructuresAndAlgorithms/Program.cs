beba5f6997bf9c41235ed99de3243036b6a64570
LukeDias <lhollowwizard@gmail.com>
2021-01-11 23:56:10 -0300

    Trying to implement Binary Tree

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -10,25 +10,6 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main()
         {
-            var linked = new MyLinkedList(0);
-            linked.Append(1);
-            linked.Append(2);
-            linked.Append(3);
-            linked.Append(4);
-            linked.Append(5);
-            linked.Append(6);
-            linked.Prepend(-1);
-            linked.Prepend(-2);
-            linked.Prepend(-3);
-            linked.Prepend(-4);
-
-            linked.Reverse();
-            var list = linked.GetAll();
-            foreach (var number in list)
-            {
-                Console.WriteLine(number);
-            }
-            Console.WriteLine();
         }
     }
 }
---
+++ DataStructuresAndAlgorithms/Tree/BinaryTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.Tree
+{
+    class BinaryTreeNode
+    {
+        public int Value;
+        public BinaryTreeNode LeftNode;
+        public BinaryTreeNode RightNode;
+
+        public BinaryTreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+    class BinaryTree
+    {
+        private BinaryTreeNode _root;
+
+        public void Insert(int value)
+        {
+            var newNode = new BinaryTreeNode(value);
+            if (_root == null)
+                _root = newNode;
+            else
+            {
+                var currentNode = _root;
+                var nextNode = (value < currentNode.Value) ? currentNode.LeftNode : currentNode.RightNode;
+                while (true)
+                {
+                    if(nextNode == null)
+                    {
+                        if (value < currentNode.Value)
+                            currentNode.LeftNode = newNode;
+                        else if (value > currentNode.Value)
+                            currentNode.RightNode = newNode;
+                        return;
+
+                    }
+                    currentNode = nextNode;
+                    nextNode = (value < currentNode.Value) ? currentNode.LeftNode : currentNode.RightNode;
+                } 
+            }
+        }
+
+        public bool LookUp(int value)
+        {
+            BinaryTreeNode currentNode = _root;
+            BinaryTreeNode nextNode;
+            while (true)
+            {
+                nextNode = (value < currentNode.Value) ? currentNode.LeftNode : currentNode.RightNode;
+                if (nextNode == null)
+                {
+                    return false;
+                }
+                else if (nextNode.Value == value)
+                {
+                    return true;
+                }
+                currentNode = nextNode;
+            }
+        }
+    }
+}
