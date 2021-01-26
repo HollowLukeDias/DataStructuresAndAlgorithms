8983328af1eebdd8fd7025524d0bc3f44343e7fe
LukeDias <lhollowwizard@gmail.com>
2021-01-26 01:48:20 -0300

    Checks the Max Depth of a Binary Tree

---
+++ DataStructuresAndAlgorithms/CodingChallenges/BinaryTreeMaxDepth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.CodingChallenges
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class BinaryTreeMaxDepth
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            var maxRight = 0;
+            var maxLeft = 0;
+            if (root == null)
+                return 0;
+
+            if (root.left == null && root.right == null)
+                return 1;
+
+            if (root.left != null)
+                maxLeft = MaxDepth(root.left) + 1;
+
+            if (root.right != null)
+                maxRight = MaxDepth(root.right) + 1;
+
+            return (maxLeft <= maxRight) ? maxLeft : maxRight;
+        }
+    }
+}
