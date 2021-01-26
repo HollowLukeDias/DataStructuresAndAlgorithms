4e5dd6b6cd8c28084f823120e46dab660c45ea2d
Lucas Marinho Dias <lucas.mdias@stone.com.br>
2021-01-26 17:20:41 -0300

    Cleaned main

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -13,15 +13,6 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main()
         {
-            var numbers = new int[] { 1, 4, 6, 12, 58, 13, 5, 3, -10, 159, 1020, 1200, 1019, 1199 };
-            numbers = Sort.MergeSort(numbers);
-            //var nums1 = new int[] { 1, 4, 7, 9, 10, 110, 150, 190, 215, 220 };
-            //var nums2 = new int[] { 3, 5, 8, 15 };
-            //var numbers = Sort.MergeSortedArray(nums1, nums2);
-            foreach (var number in numbers)
-            {
-                Console.WriteLine(number);
-            }
         }
     }
 }
