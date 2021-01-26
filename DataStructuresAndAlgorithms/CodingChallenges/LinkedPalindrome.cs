c79909ea150c742260de3f8a445927023da080d1
LukeDias <lhollowwizard@gmail.com>
2021-01-26 01:46:51 -0300

    Checking if a Linked List is a palindrome.

--- DataStructuresAndAlgorithms/CodingChallenges/LinkedPalindrome.cs
+++ DataStructuresAndAlgorithms/CodingChallenges/LinkedPalindrome.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace DataStructuresAndAlgorithms.CodingChallenges
 {
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
     class LinkedPalindrome
     {
+        public bool IsPalindrome(ListNode head)
+        {
+            List<char> chars = new List<char>();
+            while (head != null)
+            {
+                chars.Add((char)head.val);
+                head = head.next;
+            }
+            for (int i = 0; i < chars.Count / 2; i++)
+                if (chars[i] != chars[chars.Count - i - 1])
+                    return false;
+
+            return true;
+        }
     }
 }
